88c4ddbda9ab29ba1d2025bf6ec41afcd60dcff2
zhangchunming <zhangchunming@jinshuju.net>
2021-06-11 22:06:42 +0800

    Add Item Form And Card Column Components

---
+++ src/Column.tsx
@@ -0,0 +1,18 @@
+import {ColumnContainer, ColumnTitle} from "./styles";
+import React, {FC} from "react";
+import {AddNewItem} from "./AddNewItem"
+
+type ColumnProps = {
+  text?: string
+  children?: React.ReactNode
+}
+
+export const Column: FC<ColumnProps> = ({text, children}) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+      <AddNewItem toggleButtonText="+ Add another task" onAdd={console.log} dark />
+    </ColumnContainer>
+  )
+}
