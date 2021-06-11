47f521ac7316449dc00fa9b19dae71f2c0e42d22
zhangchunming <zhangchunming@jinshuju.net>
2021-06-11 22:41:59 +0800

    Get The Data From AppStateContext

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,26 @@
 import {ColumnContainer, ColumnTitle} from "./styles";
-import React, {FC} from "react";
+import React from "react";
 import {AddNewItem} from "./AddNewItem"
+import {useAppState} from "./state/AppStateContext"
+import {Card} from "./Card"
 
 type ColumnProps = {
   text?: string
-  children?: React.ReactNode
+  id: string
 }
 
-export const Column: FC<ColumnProps> = ({text, children}) => {
+export const Column = ({text, id}: ColumnProps) => {
+  const {getTasksByListId} = useAppState()
+
+  const tasks = getTasksByListId(id)
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
-      <AddNewItem toggleButtonText="+ Add another task" onAdd={console.log} dark />
+      {tasks.map(task => (
+        <Card text={task.text} key={task.id} id={task.id}/>
+      ))}
+      <AddNewItem toggleButtonText="+ Add another task" onAdd={console.log} dark/>
     </ColumnContainer>
   )
 }
